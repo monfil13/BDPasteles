a83ae914569a696e00db92ff8f6b0bfa4faeaf1e
Daniel Monfil <davilesmonfil34@gmail.com>
2023-12-03 14:41:24 -0600

    cambios

--- resources/views/partials/nav.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="/">Inicio</a>
-<a href="/login">Login</a>
-<a href="/dashboard">Dashboard</a>
-<a href="#">Logout</a>
--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,13 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
 */
 
-Route::view('/', 'welcome');
-Route::view('/login', 'login')->name('login')->middleware('guest');
-Route::view('/dashboard', 'dashboard')->middleware('auth');
-
-Route::post('/login', function(){
-$credentials = request()->only('email', 'password');
-
-if (Auth::attempt($credentials)){
-    request()->session()->regenerate();
-
-
-    return redirect ("dashboard");
-}
-return redirect ("login");
+Route::get('/', function () {
+    return view('welcome');
 });
